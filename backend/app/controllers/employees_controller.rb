df6ba41e7d20269aca6f29ee86656c8c21fe4a72
Payam Yektamaram <payamyek@gmail.com>
2025-05-31 16:04:08 -0400

    feat: add employees get endpoints

---
+++ backend/app/controllers/employees_controller.rb
@@ -0,0 +1,12 @@
+class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+    render json: @employees
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+    render json: @employee
+  end
+
+end
