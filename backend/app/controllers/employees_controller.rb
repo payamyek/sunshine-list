a497d1e90d519b7869b982c67349994e71b9cd97
Payam Yektamaram <payamyek@gmail.com>
2025-05-31 23:23:07 -0400

    feat: include associations

--- backend/app/controllers/employees_controller.rb
+++ backend/app/controllers/employees_controller.rb
@@ -1,12 +1,12 @@
 class EmployeesController < ApplicationController
   def index
     @employees = Employee.order(id: :asc).page(params[:page]).per(params[:size])
-    render json: @employees
+    render json: @employees, include: [:sector, :job_title, :employer]
   end
 
   def show
     @employee = Employee.find(params[:id])
-    render json: @employee
+    render json: @employee, include: [:sector, :job_title, :employer]
   end
 
 end
