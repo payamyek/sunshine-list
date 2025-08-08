3eeb81a6ac8c58811e488e73790a5d265fc772af
Payam Yektamaram <payamyek@gmail.com>
2025-08-07 22:39:29 -0400

    feat: add `pg_search` gem

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_05_024020) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_08_023833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -37,6 +37,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_05_024020) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_job_titles_on_name", unique: true
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "sectors", force: :cascade do |t|
